0b017cd863658408924874377e7257ec88ccf488
Yen-Jen Wang <wangyenjen@berkeley.edu>
2024-05-04 21:47:21 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     author='Xinyang Gu, Yen-Jen Wang, Jianyu Chen',
     license="BSD-3-Clause",
     packages=find_packages(),
-    author_email='zlw21gxy@gmail.com, wangyenjen@berkeley.edu',
+    author_email='zlw21gxy@gmail.com, wangyenjen@berkeley.edu, jianyuchen@tsinghua.edu.cn',
     description='Isaac Gym environments for humanoid robot',
     install_requires=['isaacgym',  # preview4
                       'wandb',
